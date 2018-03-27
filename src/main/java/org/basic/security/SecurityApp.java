6a405b511c8b5fb2efa1fef3b43fc4195c7cce5f
sagarvilas <sagar.vilas@gmail.com>
2018-03-27 07:57:14 -0400

    Update SecurityApp.java

--- src/main/java/org/basic/security/SecurityApp.java
+++ src/main/java/org/basic/security/SecurityApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class SecurityApp extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
